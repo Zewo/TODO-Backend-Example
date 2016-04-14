51761048cd7f0780c17c855f4011fc9a2001b3c6
Scott Byrns <contact@scottbyrns.com>
2016-04-13 20:29:06 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CrateIO",
+    name: "TODOBackend",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
