65bd464a527dc2aba27dcd6d0872355f2c7f72ea
Scott Byrns <contact@scottbyrns.com>
2016-04-14 00:54:44 -0600

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,74 +1,108 @@
 import HTTPServer
 import Router
 import JSON
+import JSONMediaType
+import MediaType
+import ContentNegotiationMiddleware
+import Mapper
+import StructuredData
+import Log
+import LogMiddleware
+
+struct Todo: Mappable, StructuredDataRepresentable {
+    var id: Int?
+    var title: String?
+    var url: String?
+    var completed: Bool?
+    var order: Int?
+
+    init(mapper: Mapper) throws {
+        self.id = mapper.map(optionalFrom: "id")
+        self.title = mapper.map(optionalFrom: "title")
+        self.url = mapper.map(optionalFrom: "url")
+        self.completed = mapper.map(optionalFrom: "completed")
+        self.order = mapper.map(optionalFrom: "order")
+    }
+    
+    var structuredData: StructuredData {
+        get {
+            return [
+               "id": StructuredData.from(id ?? 0),
+               "title": StructuredData.from(title ?? ""),
+               "url": StructuredData.from(url ?? ""),
+               "completed": StructuredData.from(completed ?? false),
+               "order": StructuredData.from(order ?? 0)
+            ]
+        }
+    }
+}
 
 func getIdParameter(from request: Request) -> Int {
     guard let idString = request.pathParameters["id"] else { return 0 }
     return Int(idString) ?? 0
 }
 
-let router = Router { route in
+let contentNegotiation = ContentNegotiationMiddleware(mediaTypes: [JSONMediaType()])
 
-    var todos = [JSON?]()
-    let corsHeader = Headers(["access-control-allow-origin" : Header(stringLiteral: "*")])
+let router = Router(middleware: contentNegotiation) { route in
+
+    var todos = [Todo?]()
+    let corsHeader = Headers(["access-control-allow-origin" : Header("*")])
     
-    route.get("/") { _ in
-        var todooutput = ""
-        for (index, _todo) in todos.enumerated() {
-            if var todo = _todo where _todo != nil {
-                if index > 0 { todooutput += "," }
-                todooutput += JSONSerializer().serializeToString(todo)
+    route.get("/") { request in
+        var structuredTodos = [StructuredData]()
+        for _todo in todos where _todo != nil {
+            if let todo = _todo {
+                structuredTodos.append(todo.structuredData)
             }
         }
-        return Response(body: Data(stringLiteral: "[\(todooutput)]"), headers: corsHeader)
+        return Response(status: .ok, headers: corsHeader, content: StructuredData.from(structuredTodos))
     }
     
     route.post("/") { request in
-        switch request.body {
-            case .buffer(let data):
-                var json = try JSONParser().parse(data)
-                json["url"] = JSON.from("http://localhost:34197/todo/\(todos.count)")
-                json["completed"] = JSON.from(false)
-                todos.append(json)
-                return Response(body:Data(JSONSerializer().serializeToString(json)), headers: corsHeader)
-            default: break
+        guard let content = request.content else {
+            return Response(headers: corsHeader)
         }
-        return Response(body:Data(), headers: corsHeader)
+        guard var todo = Todo.makeWith(structuredData: content) else {
+            return Response(headers: corsHeader)
+        }
+        todo.url = "http://localhost:34197/todo/\(todos.count)"
+        todo.id = todos.count
+        todos.append(todo)
+        return Response(status: .ok, headers: corsHeader, content: todo)
     }
     
     route.delete("/") { request in
-        todos = [JSON?]()
+        todos = [Todo?]()
         return Response(headers: corsHeader)
     }
     
     route.get("/todo/:id") { request in
         let index = getIdParameter(from:request)
-        guard let todo: JSON = todos[index] else {
-            return Response(body:Data(), headers: corsHeader)
+        guard let todo: Todo = todos[index] else {
+            return Response(headers: corsHeader)
         }
-        return Response(body:Data(JSONSerializer().serializeToString(todo)), headers: corsHeader)
+        return Response(status: .ok, headers: corsHeader, content: todo)
     }
     
     route.delete("/todo/:id") { request in
         let index = getIdParameter(from:request)
         todos[index] = nil
-        return Response(body:Data(), headers: corsHeader)
+        return Response(headers: corsHeader)
     }
 
     route.patch("/todo/:id") { request in
         let index = getIdParameter(from:request)
-        guard let todo: JSON = todos[index] else {
-            return Response(body:Data(), headers: corsHeader)
+        guard let content = request.content else {
+            return Response(headers: corsHeader)
         }
-        switch request.body {
-            case .buffer(let data):
-                var json = try JSONParser().parse(data)
-                json["url"] = JSON.from("http://localhost:34197/todo/\(index)")
-                todos[index] = json
-                return Response(body:Data(JSONSerializer().serializeToString(json)), headers: corsHeader)
-            default: break
+        guard var todo = Todo.makeWith(structuredData: content) else {
+            return Response(headers: corsHeader)
         }
-        return Response(body: Data(), headers: corsHeader)
+        todo.url = "http://localhost:34197/todo/\(index)"
+        todo.completed = true
+        todos[index] = todo
+        return Response(status: .ok, headers: corsHeader, content: todo)
     }
 }
 
