13101116829d72bc447d04f29d5e7d343abc7099
Scott Byrns <contact@scottbyrns.com>
2016-04-15 20:35:00 -0600

    Code cleanup, composed routes, cors middleware

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TODOBackend",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/Zewo.git", majorVersion: 0, minor: 4)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,13 +1,4 @@
-import HTTPServer
-import Router
-import JSON
-import JSONMediaType
-import MediaType
-import ContentNegotiationMiddleware
-import Mapper
-import StructuredData
-import Log
-import LogMiddleware
+import Zewo
 
 struct Todo: Mappable, StructuredDataRepresentable {
     var id: Int?
@@ -15,7 +6,7 @@ struct Todo: Mappable, StructuredDataRepresentable {
     var url: String?
     var completed: Bool?
     var order: Int?
-
+    
     init(mapper: Mapper) throws {
         self.id = mapper.map(optionalFrom: "id")
         self.title = mapper.map(optionalFrom: "title")
@@ -37,18 +28,22 @@ struct Todo: Mappable, StructuredDataRepresentable {
     }
 }
 
+struct CorsMiddleware: Middleware {
+    func respond(to request: Request, chainingTo chain: Responder) throws -> Response {
+        var response = try chain.respond(to: request)
+        response.headers.headers["access-control-allow-origin"] = Header("*")
+        return response
+    }
+}
+
 func getIdParameter(from request: Request) -> Int {
     guard let idString = request.pathParameters["id"] else { return 0 }
     return Int(idString) ?? 0
 }
 
-let contentNegotiation = ContentNegotiationMiddleware(mediaTypes: [JSONMediaType()])
+var todos = [Todo?]()
 
-let router = Router(middleware: contentNegotiation) { route in
-
-    var todos = [Todo?]()
-    let corsHeader = Headers(["access-control-allow-origin" : Header("*")])
-    
+let listRouter = Router() { route in
     route.get("/") { request in
         var structuredTodos = [StructuredData]()
         for _todo in todos where _todo != nil {
@@ -56,54 +51,55 @@ let router = Router(middleware: contentNegotiation) { route in
                 structuredTodos.append(todo.structuredData)
             }
         }
-        return Response(status: .ok, headers: corsHeader, content: StructuredData.from(structuredTodos))
+        return Response(status: .ok, content: StructuredData.from(structuredTodos))
     }
     
     route.post("/") { request in
-        guard let content = request.content else {
-            return Response(headers: corsHeader)
-        }
-        guard var todo = Todo.makeWith(structuredData: content) else {
-            return Response(headers: corsHeader)
-        }
+        guard let content = request.content else { return Response() }
+        guard var todo = Todo.makeWith(structuredData: content) else { return Response() }
+        
         todo.url = "http://localhost:34197/todo/\(todos.count)"
         todo.id = todos.count
         todos.append(todo)
-        return Response(status: .ok, headers: corsHeader, content: todo)
+        return Response(status: .ok, content: todo)
     }
     
     route.delete("/") { request in
         todos = [Todo?]()
-        return Response(headers: corsHeader)
+        return Response()
     }
-    
-    route.get("/todo/:id") { request in
+}
+
+let todoRouter = Router() { route in
+    route.get("/") { request in
         let index = getIdParameter(from:request)
-        guard let todo: Todo = todos[index] else {
-            return Response(headers: corsHeader)
-        }
-        return Response(status: .ok, headers: corsHeader, content: todo)
+        guard let todo: Todo = todos[index] else { return Response() }
+        
+        return Response(status: .ok, content: todo)
     }
     
-    route.delete("/todo/:id") { request in
+    route.delete("/") { request in
         let index = getIdParameter(from:request)
         todos[index] = nil
-        return Response(headers: corsHeader)
+        return Response()
     }
-
-    route.patch("/todo/:id") { request in
+    
+    route.patch("/") { request in
         let index = getIdParameter(from:request)
-        guard let content = request.content else {
-            return Response(headers: corsHeader)
-        }
-        guard var todo = Todo.makeWith(structuredData: content) else {
-            return Response(headers: corsHeader)
-        }
+        guard let content = request.content else { return Response() }
+        guard var todo = Todo.makeWith(structuredData: content) else { return Response() }
+        
         todo.url = "http://localhost:34197/todo/\(index)"
         todo.completed = true
         todos[index] = todo
-        return Response(status: .ok, headers: corsHeader, content: todo)
+        return Response(status: .ok, content: todo)
     }
+}
+
+let contentNegotiationMiddleware = ContentNegotiationMiddleware(mediaTypes: [JSONMediaType()])
+let router = Router(middleware: contentNegotiationMiddleware, CorsMiddleware()) { route in
+    route.compose(path: "/", router: listRouter)
+    route.compose(path: "/todo/:id", router: todoRouter)
 }
 
 try Server(on: 34197, responder: router).start()
