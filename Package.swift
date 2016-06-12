1dcf2160ad701df46464c7f8c02bf41c1a978980
Dan Appel <dan.appel00@gmail.com>
2016-06-12 15:58:30 -0700

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "TodoBackend",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 7),
-        .Package(url: "../Resource", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/LogMiddleware", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/JSONMediaType", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/Resource.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 7),
     ]
 )
