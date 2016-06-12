3187ba0a9ced254698d71a656737296f2a24ea4f
Dan Appel <dan.appel00@gmail.com>
2016-06-11 20:20:51 -0700

    Make file headers MIT

--- Sources/TodoResource.swift
+++ Sources/TodoResource.swift
@@ -1,10 +1,26 @@
+// TodoResource.swift
 //
-//  TodoResource.swift
-//  TODOBackend
+// The MIT License (MIT)
 //
-//  Created by Dan Appel on 5/1/16.
+// Copyright (c) 2015 Zewo
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import Resource
 import JSONMediaType
@@ -25,6 +41,8 @@ extension StructuredData {
 
 func makeTodoResource(store todoStore: TodoStore) -> Resource {
 
+    let apiRoot = "http://127.0.0.1:8080"
+
     return Resource(mediaTypes: JSONMediaType()) { todo in
 
         // GET /
@@ -36,7 +54,7 @@ func makeTodoResource(store todoStore: TodoStore) -> Resource {
         // POST /
         todo.post { (request: Request, todo: Todo) in
             let id = todoStore.nextId()
-            let newTodo = todo.modify(id: id, url: "http://127.0.0.1:8080/todos/\(id)")
+            let newTodo = todo.modify(id: id, url: "\(apiRoot)/\(id)")
             todoStore.insert(todo: newTodo, id: id)
             return Response(content: newTodo)
         }
